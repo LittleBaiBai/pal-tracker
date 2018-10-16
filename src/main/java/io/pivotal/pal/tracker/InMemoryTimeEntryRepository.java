c04f8aa979b376da1a0d9d5cf0d557fd92d49384
Yuxin Bai <ybai@pivotal.io>
2018-10-16 12:02:34 -0400

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -9,26 +9,31 @@ public class InMemoryTimeEntryRepository implements TimeEntryRepository {
     private long index = 0;
     private Map<Long, TimeEntry> timeEntries = new HashMap<>();
 
+    @Override
     public TimeEntry create(TimeEntry timeEntry) {
         timeEntry.setId(++index);
         timeEntries.put(timeEntry.getId(), timeEntry);
         return timeEntry;
     }
 
+    @Override
     public TimeEntry find(long id) {
         return timeEntries.get(id);
     }
 
+    @Override
     public List<TimeEntry> list() {
         return new ArrayList<>(timeEntries.values());
     }
 
+    @Override
     public TimeEntry update(long id, TimeEntry timeEntry) {
         timeEntry.setId(id);
         timeEntries.put(id, timeEntry);
         return timeEntry;
     }
 
+    @Override
     public TimeEntry delete(long id) {
         return timeEntries.remove(id);
     }
